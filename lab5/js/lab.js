7840c0c1cd0769abd71379e7bd6641af2e6b7f40
LPKJW <lars.widman@hotmail.com>
2024-04-25 21:41:42 -0700

    Finished Lab 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,20 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 5
+// Author: Lars Widman
+// Date: 4/25/24
+// Licence: Public Domain
 
-// Constants
+// Define variables
+var make = "Ford";
+var model = "Taurus";
+var color = "Rusty";
+var year = 1995;
 
-// Functions
+// calculate
+var age = 2024 - year;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
